867ad12b50749f94976e16d2f4898908fbc80a37
Jeff Malnick <malnick@gmail.com>
2016-10-28 08:38:34 -0700

    server

--- server.go
+++ server.go
@@ -9,17 +9,27 @@ import (
 )
 
 func TestHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Transfer-Encoding", "chunked")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+
 	closed := w.(http.CloseNotifier).CloseNotify()
+	last_msg := "unset"
+	flusher, _ := w.(http.Flusher)
 	for {
 		select {
 		case <-closed:
 			fmt.Println("Client closed connection")
+			fmt.Printf("Last Message: %s\n", last_msg)
 			return
 		default:
 			t, _ := time.Now().MarshalJSON()
-			fmt.Println("Sending: %s", t)
+			last_msg = string(t)
 			copy(t, "\n")
 			w.Write([]byte(t))
+
+			flusher.Flush()
+			time.Sleep(1 * time.Second)
 		}
 	}
 }
@@ -27,8 +37,6 @@ func TestHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/test", TestHandler)
-	r.Headers("Transfer-Encoding", "chunked")
-	r.Headers("Connection", "keep-alive")
 	fmt.Println("Running tester on :8000")
 	http.ListenAndServe(":8000", r)
 }
